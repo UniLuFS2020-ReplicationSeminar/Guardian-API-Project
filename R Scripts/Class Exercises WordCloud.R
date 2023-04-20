65a9c77922bb0b6b78738ab08f30fc800f98ac1b
Amanda Franklin-Ryan <amanda.franklin.ryan@gmail.com>
2023-04-20 13:53:20 +0200

    Finished wordcloud code

--- R Scripts/Class Exercises WordCloud.R
+++ R Scripts/Class Exercises WordCloud.R
@@ -1,15 +1,14 @@
 library(dplyr)
 library(quanteda) # for text analysis
 library(SnowballC) # for removing stopwords
-library(lubridate)
+library(lubridate) # for manipulating dates
 library(rio)
-library(quanteda.textplots)
+library(quanteda.textplots) # for plotting wordcloud
 
 # Import Syrian headlines and convert to corpus
 syria_headlines_dataframe <- readRDS(file = "Data files/syrian_headlines_dataframe.rds")
 
-###--- Create worldcloud with key words from the corpus
-
+###--- Create word cloud with key words from the corpus
 current_corpus <- quanteda::corpus(syria_headlines_dataframe, text_field="headline_vector")
 
 # Add more words to stop word dictionary
@@ -19,8 +18,10 @@ custom_dictionary <- append(custom_dictionary,c("review", "video", "syria", "edi
 tokens_cleaned  <- tokens(current_corpus, remove_punct = TRUE, remove_numbers=TRUE, remove_symbols = TRUE, split_hyphens = TRUE, 
                           remove_separators = TRUE, remove_url=TRUE)
 
+# Remove stopwords
 no_stopwords <- tokens_remove(tokens_cleaned, custom_dictionary)
 
+# Identify 400 most common bigrams and combine then e.g. David Cameron, Al-Assad etc.
 toks_ngram <- tokens_ngrams(no_stopwords, n = 2:2, concatenator = " ")
 bigrams_dfm <- dfm(toks_ngram)
 list <- as.list(topfeatures(bigrams_dfm , 400))
@@ -29,10 +30,11 @@ names <- names(list)
 combined_tokens_list <- phrase(names)
 tokens_combined <- tokens_compound(no_stopwords,combined_tokens_list, case_insensitive=TRUE)
 
+# Create document ferquency matrix and trim so that each term must be mentioned at least 3 times in total
 cleaned_dfm <- dfm(tokens_combined) %>% 
   dfm_trim(min_termfreq = 3)
 
-
+# Plot wordcloud
 set.seed(123) # ensure get same wordcloud every time
 syria_wordcloud <- textplot_wordcloud(cleaned_dfm , max_words=100, rotation= 0.25, color = RColorBrewer::brewer.pal(8,"Dark2"))
 
